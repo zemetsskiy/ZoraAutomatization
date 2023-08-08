629c8b8b4e93d56e028f836f613f3d216c65ae87
zemetsskiy <yaroslavzhemerikin_@outlook.com>
2023-08-08 21:53:19 +0300

    get_current_gwei func added

--- Bridger.py
+++ Bridger.py
@@ -1,4 +1,8 @@
+import math
+
 from web3 import Web3
+from config.logger import logger
+from config.rpcs import rpcs
 
 
 class Bridger:
@@ -6,7 +10,18 @@ class Bridger:
         pass
 
     def bridge(self):
-        pass
+       pass
 
     def get_current_gwei(self):
-        pass
+        web3 = Web3(Web3.HTTPProvider(rpcs["eth"]))
+        try:
+            gas_price = web3.eth.gas_price
+            print("GasPrice: ", round(gas_price / 10**9))
+            return gas_price
+        except Exception as err:
+            print("Ошибка при получении цены газа:", err)
+            return None
+
+if __name__ == "__main__":
+    Bridger = Bridger()
+    print(Bridger.get_current_gwei())
