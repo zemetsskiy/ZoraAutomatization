00ef9fc5f750161c3f80407df9642cd2f17d5f72
zemetsskiy <yaroslavzhemerikin_@outlook.com>
2023-08-11 19:48:16 +0300

    warm_up method added

--- Minter.py
+++ Minter.py
@@ -1,11 +1,13 @@
 import asyncio
 import re
+import random
+import string
 import web3.exceptions
 
 
 from web3 import Web3
 from logger import logger
-from config import rpcs, nft_contract_abi, nft_ZoraCreator_contract_abi, ZoraNFTCreator_contract_abi
+from config import rpcs, nft_contract_abi, nft_ZoraCreator_contract_abi, ZoraNFTCreator_contract_abi, JSONExtensionRegistry_contract_abi
 
 
 class Minter:
@@ -200,3 +202,61 @@ class Minter:
                 logger.error(f"Insufficient funds for gas * price + value.")
             else:
                 logger.error(f"Something went wrong: {err}")
+
+    async def walletWarmUp(self, nft_collection_address, uri):
+        web3 = Web3(Web3.HTTPProvider(rpcs["zora"], request_kwargs={
+            'proxies': {'https': 'http://' + "pnorwyha:snmfocltb81h@209.99.165.189:6094",
+                        'http': 'http://' + "pnorwyha:snmfocltb81h@209.99.165.189:6094"}}))
+        logger.info(f"Successfully connected to {rpcs['zora']}")
+
+        wallet_address = web3.eth.account.from_key(self.pk).address
+        wallet_balance = web3.eth.get_balance(wallet_address)
+
+        logger.info(f"Wallet address: {wallet_address}")
+        logger.info(f"Balance in ZORA network: {web3.from_wei(wallet_balance, 'ether')}")
+
+        try:
+            contract = web3.eth.contract(address=Web3.to_checksum_address("0xABCDEFEd93200601e1dFe26D6644758801D732E8"),
+                                         abi=JSONExtensionRegistry_contract_abi)
+
+            warm_tx = contract.functions.setJSONExtension(
+                    target=Web3.to_checksum_address(nft_collection_address),
+                    uri=uri
+            ).build_transaction({
+                'from': web3.to_checksum_address(wallet_address),
+                'nonce': web3.eth.get_transaction_count(wallet_address),
+                'maxPriorityFeePerGas': web3.to_wei(0.005, 'gwei'),
+                'maxFeePerGas': web3.to_wei(0.005, 'gwei')
+            })
+
+            signed_warm_tx = web3.eth.account.sign_transaction(warm_tx, self.pk)
+            raw_warm_tx_hash = web3.eth.send_raw_transaction(signed_warm_tx.rawTransaction)
+            warm_tx_hash = web3.to_hex(raw_warm_tx_hash)
+
+            logger.info(f"Warming up tx hash: {warm_tx_hash}")
+
+            for i in range(5):
+                await asyncio.sleep(5)
+                try:
+                    create_tx_receipt = web3.eth.wait_for_transaction_receipt(raw_warm_tx_hash, timeout=300)
+                    if create_tx_receipt.status == 1:
+                        logger.info(f"Transaction: https://explorer.zora.energy/tx/{warm_tx_hash}")
+                    else:
+                        logger.error("Something went wrong while  warming up")
+                except web3.exceptions.TransactionNotFound as err:
+                    logger.error(f"Something went wrong while  warming up: {err}")
+                    continue
+                except Exception as err:
+                    logger.error(f"Something went wrong while warming up: {err}")
+
+        except Exception as err:
+            if "insufficient funds" in str(err):
+                logger.error(f"Insufficient funds for gas * price + value.")
+            else:
+                logger.error(f"Something went wrong: {err}")
+
+    @staticmethod
+    def generateUri(length=50, prefix="baf"):
+        characters = string.ascii_lowercase + string.digits
+        random_chars = ''.join(random.choice(characters) for _ in range(length - len(prefix)))
+        return prefix + random_chars
